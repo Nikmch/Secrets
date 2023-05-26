c41acee32ef77c24b3443b0db6104912c16a557d
Nikmch <107665487+Nikmch@users.noreply.github.com>
2023-05-26 17:31:52 +1000

    bcrypt
    using bcrypt with 10 rounds of salting

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app =express();
 
@@ -23,7 +24,6 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -42,9 +42,11 @@ app.get("/register", function(req, res){
 
 
 app.post("/register", function(req, res){
+
+bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: hash
     });
        newUser.save()
     .then(function(){
@@ -54,6 +56,7 @@ app.post("/register", function(req, res){
         console.log(err);
     })
 });
+});
 
 
 app.post("/login", function(req,res){
@@ -62,9 +65,11 @@ app.post("/login", function(req,res){
  
     User.findOne({email: username})
     .then(function(foundUser){
-        if(foundUser.password ===password){
-            res.render("secrets");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+    if (result === true){
+ res.render("secrets");
+    }
+})
     })
     .catch(function(err){
         console.log(err);
